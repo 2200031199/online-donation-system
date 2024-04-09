8286ff5ce2c2d3c6a2460268d04a501c0e2a2417
likith <withlikith@gmail.com>
2024-04-09 14:52:04 +0530

    first commit

---
+++ backendapp/Server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const dburl = "mongodb://localhost:27017/donationsystem"
+mongoose.connect(dburl).then(() => {
+    console.log("Connected to DB Successfully")
+}).catch((err) => {
+    console.log(err.message) 
+});
+
+const app = express() 
+app.use(cors())
+app.use(express.json()) 
+
+const donorrouter = require("./routes/donorroute")
+const adminrouter = require('./routes/adminroutes')
+
+app.use("",donorrouter)
+app.use("",adminrouter)
+
+
+const port = 2024
+app.listen( port, () =>{
+    console.log(`Server is Running at ${port}`)
+})
